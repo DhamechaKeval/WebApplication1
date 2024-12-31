31b1386bc00305d28fc950966b40921ab45f0630
KevalDhamecha <kdhamecha541@rku.ac.in>
2024-12-31 11:40:37 +0530

    Choose file and upload functionality

---
+++ fill_upload.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="fill_upload.aspx.cs" Inherits="WebApplication1.fill_upload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click1" Text="Button" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ fill_upload.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class fill_upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                try
+                {
+                    string Filename = FileUpload1.FileName;
+                    string path = Server.MapPath("~/FILES/") + Filename;
+                    FileUpload1.SaveAs(path);
+                    Label1.Text = "File uploaded";
+                }
+                catch (Exception ex )
+                {
+                    Label1.Text = ex.Message;
+                }
+            }
+        }
+    }
+}
